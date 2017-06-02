6de06ef828c98ff9305df37aeab011fa9becce8c
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2017-06-02 13:51:45 +0900

    fix lint error

--- src/index.js
+++ src/index.js
@@ -7,22 +7,22 @@ import languages from './constants/languages';
 import './styles/index.scss';
 
 // Teach Autosuggest how to calculate suggestions for any given input value.
-const getSuggestions = value => {
+const getSuggestions = (value) => {
   const inputValue = value.trim().toLowerCase();
   const inputLength = inputValue.length;
 
-  return inputLength === 0 ? [] : languages.filter(lang =>
-    lang.name.toLowerCase().slice(0, inputLength) === inputValue
+  return inputLength === 0 ? [] : languages.filter((lang) =>
+    lang.name.toLowerCase().slice(0, inputLength) === inputValue,
   );
 };
 
 // When suggestion is clicked, Autosuggest needs to populate the input element
 // based on the clicked suggestion. Teach Autosuggest how to calculate the
 // input value for every given suggestion.
-const getSuggestionValue = suggestion => suggestion.name;
+const getSuggestionValue = (suggestion) => suggestion.name;
 
 // Use your imagination to render suggestions.
-const renderSuggestion = suggestion => (
+const renderSuggestion = (suggestion) => (
   <div>
     {suggestion.name}
   </div>
@@ -39,13 +39,13 @@ class Example extends React.Component {
     // and they are initially empty because the Autosuggest is closed.
     this.state = {
       value: '',
-      suggestions: []
+      suggestions: [],
     };
   }
 
   onChange = (event, { newValue }) => {
     this.setState({
-      value: newValue
+      value: newValue,
     });
   };
 
@@ -53,14 +53,14 @@ class Example extends React.Component {
   // You already implemented this logic above, so just use it.
   onSuggestionsFetchRequested = ({ value }) => {
     this.setState({
-      suggestions: getSuggestions(value)
+      suggestions: getSuggestions(value),
     });
   };
 
   // Autosuggest will call this function every time you need to clear suggestions.
   onSuggestionsClearRequested = () => {
     this.setState({
-      suggestions: []
+      suggestions: [],
     });
   };
 
@@ -71,7 +71,7 @@ class Example extends React.Component {
     const inputProps = {
       placeholder: 'Type a programming language',
       value,
-      onChange: this.onChange
+      onChange: this.onChange,
     };
 
     // Finally, render it!
@@ -89,6 +89,6 @@ class Example extends React.Component {
 }
 
 render(
-  <Example/>,
+  <Example />,
   document.getElementById('root'),
 );
